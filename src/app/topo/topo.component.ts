26ed3248014a866e829e8ecfb27951cc170d1991
Done Ribeiro <www.done_@hotmail.com>
2022-03-31 16:03:07 -0300

    topo layout

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,8 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  // template: `<p>Esse é o componente Topo</p>`
-  // styles: [`.exemplo { color: red }`]
   styleUrls: ['./topo.component.css']
 })
 export class TopoComponent {}
